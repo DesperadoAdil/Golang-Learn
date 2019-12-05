cc632c9c76c0ddf07c8ca0b43b570010e204f96f
Adil <1298939544@qq.com>
2019-12-05 20:08:08 +0800

    embedded type learning.

---
+++ codes/embedded_type.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "fmt"
+)
+
+type user struct {
+    name string
+    email string
+}
+
+func (u *user) notify()  {
+    fmt.Printf("Sending email to %s<%s>\n", u.name, u.email)
+}
+
+type admin struct {
+    user
+    level string
+}
+
+func (a *admin) notify()  {
+    fmt.Printf("Sending email to <%s>user: %s<%s>\n", a.level, a.name, a.email)
+}
+
+func main()  {
+    ad := admin{
+        user: user{
+            name: "adil",
+            email: "adil@email.com",
+        },
+        level: "admin",
+    }
+    ad.user.notify()
+    ad.notify()
+}
