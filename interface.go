2a35831e3400d696f222181b229c8c6a3c92a26e
Adil <1298939544@qq.com>
2019-12-05 19:03:08 +0800

    2019.12.5.

---
+++ interface.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type notifier interface {
+    notify()
+}
+
+type user struct {
+    name string
+    email string
+}
+
+/*
+// cannot use u (type user) as type notifier in argument to sendNotification:
+//    user does not implement notifier (notify method has pointer receiver)
+
+func (u *user) notify()  {
+    fmt.Printf("*Sending email to %s<%s>\n", u.name, u.email)
+}
+*/
+
+func (u user) notify()  {
+    fmt.Printf("Sending email to %s<%s>\n", u.name, u.email)
+}
+
+func main()  {
+    u := &user{"adil", "1298939544@qq.com"}
+    sendNotification(u)
+}
+
+func sendNotification(n notifier)  {
+    n.notify()
+}
---
+++ polymorphism.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type notifier interface {
+    notify()
+}
+
+type user struct {
+    name string
+    email string
+}
+
+func (u *user) notify()  {
+    fmt.Printf("Sending email to %s<%s>\n", u.name, u.email)
+}
+
+type admin struct {
+    name string
+    email string
+}
+
+func (a *admin) notify()  {
+    fmt.Printf("Sending admin to %s<%s>\n", a.name, a.email)
+}
+
+func main()  {
+    u := user{"adil", "1298939544@qq.com"}
+    sendNotification(&u)
+
+    a := admin{"admin", "admin@email.com"}
+    sendNotification(&a)
+}
+
+func sendNotification(n notifier)  {
+    n.notify()
+}
